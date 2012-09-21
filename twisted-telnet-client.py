e6987a5d252fc32ab8bfd75a915cc92a5686a14c
Sindre Fjogstad <sindre@fjogstad.name>
2012-09-21 16:45:55 -0500

    minor clean up of the telnet client example

--- twisted-telnet-client.py
+++ twisted-telnet-client.py
@@ -6,7 +6,6 @@
 # * ----------------------------------------------------------------------------
 # 
 
-
 import re
 import logging
 
@@ -17,13 +16,28 @@ from twisted.conch.telnet import TelnetTransport, StatefulTelnetProtocol
 
 log = logging.getLogger('TelnetClient')
 
-def connect_telnet(host,port,username,password,prompt='$',timeout=30):
+def connect_telnet(host,port,username,password,greeting,prompt='$',timeout=30):
     endpoint = TCP4ClientEndpoint(reactor, host, port,timeout)
-    telnetFactory = TelnetFactory(username,password,prompt)
+    telnetFactory = TelnetFactory(username,password,greeting,prompt)
     telnetFactory.protocol = TelnetClient
     d = endpoint.connect(telnetFactory)
+
+    def check_connection_state(transport):
+        """Since we can't use the telnet connection before we have
+        logged in and the client is in line_mode 1
+        we pause the deffered here until the client is ready
+        The client unpuase the defferer when wee are logged in
+        """
+        if transport.protocol.line_mode == 1:
+            return transport
+        d.pause()
+        transport.protocol.connected_deffered= d
+        return transport
+    d.addCallback(check_connection_state)
+
     def connection_failed(reason):
         raise TelnetConnectionError(reason)
+    
     d.addErrback(connection_failed)
     return d
 
@@ -31,8 +45,13 @@ class TelnetConnectionError(Exception):
     pass
 
 class TelnetClient(StatefulTelnetProtocol):
-    output_buffer = ''
+    output_buffer = []
     search_output = []
+
+    # The deffered from the connect endpoint method.
+    # We need to tell the deffered that it can continue when we are
+    # ready ant when the socket are connected
+    connected_deffered = None
 
     def rawDataReceived(self,bytes):
         """The login and password prompt on some systems are not
@@ -47,14 +66,17 @@ class TelnetClient(StatefulTelnetProtocol):
         else:
             self.re_prompt = re.compile(self.factory.prompt)
 
-        #print 'Received raw data:', repr(bytes)
-        if re.search('([L|l]ogin:\s+$)', bytes):
+        print 'Received raw data:', repr(bytes)
+        log.debug('Received raw telnet data: %s' % repr(bytes))
+        if re.search('([Ll]ogin:\s+$)', bytes):
             self.sendLine(self.factory.username)
-        elif re.search('([P|p]assword:\s+$)', bytes):
-            self.sendLine(self.factory.password)
-        elif self.re_prompt.search(bytes):
-            self.setLineMode(bytes)
-        self.output_buffer += bytes
+        elif re.search('([Pp]assword:\s+$)', bytes):
+            self.sendLine(str(self.factory.password))
+        elif self.re_prompt.search(bytes) or re.search(self.factory.greeting, bytes):
+            log.debug('Telnet client logged in. We are ready for commands')
+            self.setLineMode()
+            if self.connected_deffered:
+                self.connected_deffered.unpause()
 
     def connectionMade(self):
         """ Set rawMode since we do not reveice the 
@@ -64,74 +86,139 @@ class TelnetClient(StatefulTelnetProtocol):
         self.setRawMode()
 
     def lineReceived(self, line):
+        log.debug('Received telnet line: %s' % repr(line))
         if len(self.search_output) == 0:
-            self.output_buffer += line + '\n'
+            self.output_buffer.append(line)
             return
 
-        re_expected, search_deffered = self.search_output[0]
+        re_expected = self.search_output[0][0]
+        search_deffered = self.search_output[0][1]
         if not search_deffered.called:
             match = re_expected.search(line)
             if match:
-                data = self.output_buffer + line[:match.end()]
-                self.clearBuffer()
+                data = '\n'.join(self.output_buffer)
+                data += '\n%s' % line[:match.end()]
                 self.search_output.pop(0)
+                # Start the timeout of the next epect message
+                if len(self.search_output) > 0:
+                    re_expected = self.search_output[0][0]
+                    search_deffered = self.search_output[0][1]
+                    timeout = self.search_output[0][3]
+                    self.search_output[0][2] = self.getTimeoutDefer(timeout,
+                                        search_deffered, re_expected)
                 search_deffered.callback(data)
+                log.debug('clear telnet buffer in lineReceived. We have a match: %s' % line)
+                self.clearBuffer()
                 return
 
-        self.output_buffer += line + '\n'
+        self.output_buffer.append(line)
 
-    def clearBuffer(self):
+    def clearBuffer(self,to_line=-1):
         self.clearLineBuffer()
-        self.output_buffer = ''
+        if to_line == -1:
+            self.output_buffer = []
+        else:
+            self.output_buffer = self.output_buffer[to_line:]
+
+    def getTimeoutDefer(self,timeout,expect_deferred,re_expected):
+        """ Create the cancel expect messages deffer that we use to 
+        to timeout the expect deffer.
+        """
+        def expectTimedOut(expect_deferred):
+            result = ''
+            # we only test the buffer against the oldest wait. Therfore
+            # do not clear the buffer if a older exoect still waits for
+            # a messages. This should probaly rause an exceotion or somethine...
+
+            if expect_deferred.called:
+                log.error('Expected message defferer "%s" is already called. ' % expect_deferred)
+                log.error('This is a sign that something is wrong with the telnet client')
+                return
+
+            if self.search_output[0][1] == expect_deferred:
+                # set the result since this is the current search
+                result = '\n'.join(self.output_buffer)
+                self.clearBuffer()
+                self.search_output.pop(0)
+                # Start the timeout of the next epect message
+                if len(self.search_output) > 0:
+                    next_re_expected = self.search_output[0][0]
+                    next_search_deffered = self.search_output[0][1]
+                    next_timeout = self.search_output[0][3]
+                    self.search_output[0][2] = self.getTimeoutDefer(next_timeout,
+                                        next_search_deffered, next_re_expected)
+            else:
+                msg = 'Search for message "%s" timed ' % re_expected.pattern
+                msg += 'out before the search started.'
+                log.error(msg)
+                msg = 'We are currently searhing for: '
+                msg += '"%s". ' % self.search_output[0][0].pattern
+                log.error(msg)
+                index = 1
+                while index <= len(self.output_buffer):
+                    if self.search_output[index][1] == expect_deferred:
+                        self.search_output.pop(index)
+                    index += 1
+
+            expect_deferred.callback(result)
+
+        cancel_deferred = reactor.callLater(timeout,expectTimedOut,expect_deferred)
+
+        def cancelTimeout(result):
+            if not cancel_deferred.cancelled and not cancel_deferred.called:
+                cancel_deferred.cancel()
+            return result
+
+        expect_deferred.addCallback(cancelTimeout)
+        return cancel_deferred
 
     def write(self,command):
         return self.sendLine(command)
 
     def expect(self,expected,timeout=5):
         re_expected = re.compile(expected)
-        expect_deffered = defer.Deferred()
+        expect_deferred = defer.Deferred()
 
-        def expectTimedOut():
-            self.search_output.pop(0)
-            expect_deffered.callback(self.output_buffer)
-            self.clearBuffer()
+        if len(self.search_output) == 0:
+            data = ''
+            for line in self.output_buffer:
+                match = re_expected.search(line)
+                if match:
+                    data += line[:match.end()]
+                    log.debug('Match "%s" found in (%s), clear buffer' % (expected,line))
+                    self.clearBuffer()
+                    expect_deferred.callback(data)
+                    break
+                data += line + '\n'
+        else:
+            # We are not allow to start the timeout until we have started the search
+            expect_deferred.pause()
 
-        cancel_defered = reactor.callLater(timeout,expectTimedOut)
+        if not expect_deferred.called:
+            cancel_deferred = None
+            # Only create the cancel_deferred if this is current search.
+            # if we are not we risk that the search times out before the search
+            # started
+            if len(self.search_output) == 0:
+                cancel_deferred = self.getTimeoutDefer(timeout,expect_deferred,re_expected)
+            self.search_output.append([re_expected,expect_deferred,cancel_deferred,timeout])
 
-        def cancelTimeout(result):
-            if not cancel_defered.cancelled and not cancel_defered.called:
-                cancel_defered.cancel()
-            return result
-
-        expect_deffered.addCallback(cancelTimeout)
-
-        data = ''
-        for line in self.output_buffer.split('\n'):
-            match = re_expected.search(line)
-            if match:
-                data += line[:match.end()]
-                self.clearBuffer()
-                expect_deffered.callback(data)
-                break
-            data += line + '\n'
-
-        if not expect_deffered.called:
-            self.search_output.append((re_expected,expect_deffered))
-
-        return expect_deffered
+        return expect_deferred
 
     def close(self):
         self.sendLine(self.factory.logout_command)
         self.factory.transport.loseConnection()
+        return True
 
 
 class TelnetFactory(ReconnectingClientFactory):
     logout_command = 'exit'
 
-    def __init__(self,username,password,prompt='$'):
+    def __init__(self,username,password,greeting='',prompt='$'):
         self.username = username
         self.password = password
         self.prompt = prompt
+        self.greeting = greeting
         self.transport = None
 
     def buildProtocol(self, addr):
@@ -179,7 +266,8 @@ if __name__ == '__main__':
     port = 23
     username = sys.argv[2]
     password = sys.argv[3]
-    prompt = '$'
+    prompt = '\$'
+    print username, password, host
     whenConnected = connect_telnet(host,port,username,password,prompt)
     whenConnected.addCallback(testProtocol)
     reactor.run()
